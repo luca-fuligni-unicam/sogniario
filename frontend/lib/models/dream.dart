d4b9b331649aa3c84959f5efdd682490700294b8
popoff <pocariflavio@gmail.com>
2021-03-27 23:24:22 +0100

    Modified model of the Dream.

--- frontend/lib/models/dream.dart
+++ frontend/lib/models/dream.dart
@@ -1,12 +1,27 @@
 
 class Dream {
 
-  String dream;
-  DateTime registered;
+  final String id;
+  final String dream;
+  final DateTime registered;
 
   Dream({
+    this.id,
     this.dream,
-    this.registered
+    this.registered,
   });
+
+
+  factory Dream.fromJson(Map<String, dynamic> json) {
+    return Dream(
+      id: json['id'],
+      dream: json['text'],
+      registered: DateTime.parse(json['registered'])
+    );
+  }
+
+  Map<String, dynamic> dreamReport() => {
+    'text': dream,
+  };
 
 }
