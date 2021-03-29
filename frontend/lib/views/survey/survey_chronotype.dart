3b8e895216d4b1d8af2fd352b27ea6fd855cbe75
popoff <pocariflavio@gmail.com>
2021-03-29 23:08:34 +0200

    Used GET's on 'survey_chronotype' and 'survey_dream' pages to complete the surveys.

--- frontend/lib/views/survey/survey_chronotype.dart
+++ frontend/lib/views/survey/survey_chronotype.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:frontend/models/survey.dart';
+import 'package:frontend/services/rest_api/suvery_api.dart';
+import 'package:frontend/widgets/survey_card.dart';
 
 
 class SurveyChronotype extends StatefulWidget {
@@ -11,6 +14,7 @@ class SurveyChronotype extends StatefulWidget {
 class _SurveyChronotypeState extends State<SurveyChronotype> {
 
   PageController controller = PageController();
+  SurveyApi surveyApi;
   int currentIndex = 0;
 
   TextStyle _questionStyle = TextStyle(
@@ -19,13 +23,13 @@ class _SurveyChronotypeState extends State<SurveyChronotype> {
       color: Colors.black87
   );
 
-  Map<String, List<String>> questions = {
-    'A che ora preferiresti alzarti, più o meno, se fossi completamente libero di programmare la tua giornata?': ['5 - 6.30 del mattino.', '6.30 - 7.45 del mattino.', '7.45 - 9.45 del mattino.', '9.45 - 11 del mattino.', '11 - 12 del mattino.'],
-    'A che ora preferiresti andare a letto, più o meno, se fossi completamente libero di programmare la tua serata?': ['8 - 9 di sera.', '9 - 10.15 di sera.', '10.15 - 00.30 di notte.', '00.30 - 1.45 di notte.', '1.45 - 3 di notte.'],
-    'Se di solito ti devi alzare ad una certa ora del mattino, hai proprio bisogno di sentire la sveglia per svegliarti?': ['Per niente.', 'Poco.', 'Abbastanza.', 'Molto.'],
-  };
+  List<int> answers = List.generate(40, (index) => 0);
 
-  List<int> list = [0, 0, 0];
+  @override
+  void initState() {
+    surveyApi = SurveyApi();
+    super.initState();
+  }
 
 
   @override
@@ -72,92 +76,112 @@ class _SurveyChronotypeState extends State<SurveyChronotype> {
                 ),
               ),
 
-              PageView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                controller: controller,
-                onPageChanged: (index) {
-                  setState(() {
-                    currentIndex = index;
-                  });
+              FutureBuilder(
+                future: surveyApi.getSurveys('meq'),
+                builder: (context, AsyncSnapshot<Survey> data) {
+
+                  if (data.data == null) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+
+                  } else {
+
+                    if (data.data.id == null) {
+                      return Center(
+                        child: NoSurvey(
+                          child: Text('No Report!'),
+                        ),
+                      );
+                    }
+
+                    return PageView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      controller: controller,
+                      onPageChanged: (index) => setState(() => currentIndex = index),
+                      itemBuilder: (context, index) {
+                        return Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+
+                              Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Text(
+                                    data.data.questions.keys.toList()[index],
+                                    softWrap: true,
+                                    style: _questionStyle
+                                ),
+                              ),
+
+                              Divider(
+                                thickness: 1,
+                                indent: 10,
+                                endIndent: 10,
+                                color: Colors.black,
+                              ),
+
+                              for (int count = 0; count < data.data.questions.values.toList()[index].length; count++)
+                                RadioListTile(
+                                  value: count,
+                                  groupValue: answers[index],
+                                  title: Text(data.data.questions.values.toList()[index].toList()[count]),
+                                  onChanged: (changed) => setState(() => answers[index] = changed),
+                                ),
+
+                              Container(
+                                child: Center(
+                                  child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                      children: [
+
+                                        TextButton(
+                                          child: Icon(
+                                            Icons.chevron_left,
+                                            color: Colors.black,
+                                            size: 30,
+                                          ),
+                                          onPressed: () {
+                                            controller.animateToPage(controller.page.ceil() - 1,
+                                                duration: Duration(milliseconds: 750),
+                                                curve: Curves.easeInQuad
+                                            );
+                                          },
+                                        ),
+
+                                        Container(
+                                          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                          child: Text(
+                                            '${currentIndex + 1}/${data.data.questions.keys.length}',
+                                            style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                                          ),
+                                        ),
+
+                                        TextButton(
+                                          child: Icon(
+                                            Icons.chevron_right,
+                                            color: Colors.black,
+                                            size: 30,
+                                          ),
+                                          onPressed: () {
+                                            controller.animateToPage(controller.page.ceil() + 1,
+                                                duration: Duration(milliseconds: 700),
+                                                curve: Curves.easeIn);
+                                          },
+                                        ),
+
+                                      ]),
+                                ),
+                              ),
+
+                            ]);
+                      },
+                      itemCount: data.data.questions.keys.length,
+                    );
+
+                  }
                 },
-                itemBuilder: (context, index) {
-                  return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-
-                        Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                              questions.keys.toList()[index],
-                              softWrap: true,
-                              style: _questionStyle
-                          ),
-                        ),
-
-                        Divider(
-                          thickness: 1,
-                          indent: 10,
-                          endIndent: 10,
-                          color: Colors.black,
-                        ),
-
-                        for (int count = 0; count < questions.values.toList()[index].length; count++)
-                          RadioListTile(
-                            value: count,
-                            groupValue: list[index],
-                            title: Text(questions.values.toList()[index][count]),
-                            onChanged: (changed) => setState(() => list[index] = changed),
-                          ),
-
-                        Container(
-                          child: Center(
-                            child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-
-                                  TextButton(
-                                    child: Icon(
-                                      Icons.chevron_left,
-                                      color: Colors.black,
-                                      size: 30,
-                                    ),
-                                    onPressed: () {
-                                      controller.animateToPage(controller.page.ceil() - 1,
-                                          duration: Duration(milliseconds: 750),
-                                          curve: Curves.easeInQuad
-                                      );
-                                    },
-                                  ),
-
-                                  Container(
-                                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                    child: Text(
-                                      '${currentIndex + 1}/3',
-                                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
-                                    ),
-                                  ),
-
-                                  TextButton(
-                                    child: Icon(
-                                      Icons.chevron_right,
-                                      color: Colors.black,
-                                      size: 30,
-                                    ),
-                                    onPressed: () {
-                                      controller.animateToPage(controller.page.ceil() + 1,
-                                          duration: Duration(milliseconds: 700),
-                                          curve: Curves.easeIn);
-                                    },
-                                  ),
-
-                                ]),
-                          ),
-                        ),
-
-                      ]);
-                },
-                itemCount: 3,
               )
+
             ]),
       ),
     );
