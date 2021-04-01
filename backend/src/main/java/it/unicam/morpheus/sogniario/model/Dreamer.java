38c0c1fd251814b7c50f930587b053239e4a5b04
Giorgio Paoletti <giorgio.paoletti@studenti.unicam.it>
2021-04-01 12:52:21 +0200

    Added DataDiNascita e Sex to Dreamer on Model on Back End

--- backend/src/main/java/it/unicam/morpheus/sogniario/model/Dreamer.java
+++ backend/src/main/java/it/unicam/morpheus/sogniario/model/Dreamer.java
@@ -1,5 +1,6 @@
 package it.unicam.morpheus.sogniario.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -7,8 +8,11 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+
+enum Sex {MALE, FEMALE, OTHER}
 
 @Document(collection = "dreamer")
 @NoArgsConstructor
@@ -19,6 +23,13 @@ public class Dreamer {
 
     @Getter @Setter @NonNull
     private Set<CompletedSurvey> completedSurveys;
+
+    @Getter @Setter @NonNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate nascita;
+
+    @Getter @Setter @NonNull
+    private Sex sesso;
 
     public Dreamer(String id){
         if(id.isBlank()) throw new IllegalArgumentException("Id is blank");
