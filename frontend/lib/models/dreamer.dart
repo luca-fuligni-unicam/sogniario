1d4cb6fa709c6baba6be988befc27cc62713e0ac
popoff <pocariflavio@gmail.com>
2021-04-02 18:43:58 +0200

    Added new parameters in the 'Dreamer' model class for the registration.

--- frontend/lib/models/dreamer.dart
+++ frontend/lib/models/dreamer.dart
@@ -2,9 +2,21 @@
 class Dreamer {
 
   final String id;
+  final String sex;
+  final DateTime birthDate;
 
   Dreamer({
     this.id,
+    this.sex,
+    this.birthDate
   });
+
+
+  Map<String, dynamic> registered() => {
+    'id': id,
+    'completedSurveys': [],
+    'nascita': birthDate.toString().substring(0, 10),
+    'sesso': sex,
+  };
 
 }
