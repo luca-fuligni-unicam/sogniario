ae4a6fe936c62853fedae9ea7332f5a441479161
popoff <pocariflavio@gmail.com>
2021-04-06 00:07:36 +0200

    Implemented the GET that return the dreams made on a date.

---
+++ frontend/lib/services/rest_api/dream_api.dart
@@ -0,0 +1,21 @@
+import 'package:frontend/services/utils.dart';
+import 'package:frontend/models/dream.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+
+class DreamApi extends Utils {
+
+  Future<List<Dream>> getDreams(String date) async {
+    var response = await http.get(
+        Uri.tryParse('${server}api/reports/listByIdAndData/${getToken()}/$date'),
+        headers: header
+    );
+
+    return List.from(jsonDecode(response.body))
+        .map((e) => Dream.fromJson(e))
+        .toList();
+  }
+
+}
