b6c1040d11ce7efd8dbf815f4dc25d37e63ba812
popoff <pocariflavio@gmail.com>
2021-05-06 22:53:05 +0200

    Changed the CircleDecoration in base of a page.

--- frontend/lib/widgets/info_page.dart
+++ frontend/lib/widgets/info_page.dart
@@ -22,29 +22,47 @@ class InfoPage extends StatelessWidget {
           child: Stack(
               children: [
 
-                title == 'App Info' ? Positioned(
-                  top: -20,
-                  left: -100,
-                  child: CircleDecoration(
-                    height: 400,
-                    width: 400,
-                    offset: Offset(2, 2),
-                    shadow: Colors.blue.shade200,
-                    gradientOne: Colors.blue.shade50,
-                    gradientTwo: Colors.blue.shade100,
+                if (title == 'App Info')
+                  Positioned(
+                    top: -20,
+                    left: -60,
+                    child: CircleDecoration(
+                      height: 400,
+                      width: 400,
+                      offset: Offset(2, 3),
+                      shadow: Colors.blue.shade300,
+                      gradientOne: Colors.blue.shade50,
+                      gradientTwo: Colors.blue.shade100,
+                    ),
                   ),
-                ) : Positioned(
-                  top: 50,
-                  right: -100,
-                  child: CircleDecoration(
-                    height: 400,
-                    width: 400,
-                    offset: Offset(2, 2),
-                    shadow: Colors.blue.shade300,
-                    gradientOne: Colors.blue.shade100,
-                    gradientTwo: Colors.blue.shade200.withOpacity(0.8),
+
+                if (title == 'App Privacy')
+                  Positioned(
+                    top: 50,
+                    right: -80,
+                    child: CircleDecoration(
+                      height: 400,
+                      width: 400,
+                      offset: Offset(3, 2),
+                      shadow: Colors.blue.shade400,
+                      gradientOne: Colors.blue.shade100,
+                      gradientTwo: Colors.blue.shade200,
+                    ),
                   ),
-                ),
+
+                if (title == 'Quality Dream')
+                  Positioned(
+                    top: 100,
+                    right: -40,
+                    child: CircleDecoration(
+                      height: 400,
+                      width: 400,
+                      offset: Offset(0, 0),
+                      shadow: Colors.blue.shade600,
+                      gradientOne: Colors.blue.shade100,
+                      gradientTwo: Colors.blue.shade200,
+                    ),
+                  ),
 
                 ListView(
                   padding: EdgeInsets.all(10),
